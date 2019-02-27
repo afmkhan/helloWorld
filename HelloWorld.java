6015812b9754b2bcc3816258fa67dacb3aaac404
afmkhan <akhan00@stuy.edu>
2019-02-27 12:41:44 -0500

    done in class

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println("Today was very cold");
+        System.out.println("I edited this in class!");
     }
     
 }
